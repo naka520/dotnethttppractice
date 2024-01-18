d5005d7e67f267d8f01ad64ee5db84c924cc0060
naka520 <yashipipi0520@gmail.com>
2024-01-18 15:27:30 +0900

    プロジェクト ファイルを追加します。

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+public class HttpClientManager
+{
+    private static readonly HttpClient SharedClient = new HttpClient
+    {
+        BaseAddress = new Uri("https://jsonplaceholder.typicode.com"),
+    };
+
+    public static async Task Main(string[] args)
+    {
+        await GetAsync();
+    }
+
+    private static async Task GetAsync()
+    {
+        using HttpResponseMessage response = await SharedClient.GetAsync("todos/3");
+        response.EnsureSuccessStatusCode();
+
+        var jsonResponse = await response.Content.ReadAsStringAsync();
+        Console.WriteLine($"{jsonResponse}\n");
+    }
+}
