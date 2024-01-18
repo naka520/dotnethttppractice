e5303da96fdb3c747c06d24ce7e1898747f9ad0c
naka520 <yashipipi0520@gmail.com>
2024-01-18 16:04:10 +0900

    [modify]POST処理を追加

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Net.Http;
+using System.Text.Json;
+using System.Text;
 using System.Threading.Tasks;
 
 public class HttpClientManager
@@ -12,11 +14,34 @@ public class HttpClientManager
     public static async Task Main(string[] args)
     {
         await GetAsync();
+        await PostAsync();
     }
 
     private static async Task GetAsync()
     {
         using HttpResponseMessage response = await SharedClient.GetAsync("todos/3");
+        response.EnsureSuccessStatusCode();
+
+        var jsonResponse = await response.Content.ReadAsStringAsync();
+        Console.WriteLine($"{jsonResponse}\n");
+    }
+
+    static async Task PostAsync()
+    {
+        using StringContent jsonContent = new(
+            JsonSerializer.Serialize(new
+            {
+                userId = 77,
+                title = "write code sample",
+                completed = false
+            }),
+            Encoding.UTF8,
+            "application/json");
+
+        using HttpResponseMessage response = await SharedClient.PostAsync(
+            "todos",
+            jsonContent);
+
         response.EnsureSuccessStatusCode();
 
         var jsonResponse = await response.Content.ReadAsStringAsync();
