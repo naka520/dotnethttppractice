9e54633b76aa6cad5fb0c15c8e809fbd60a400ad
naka520 <yashipipi0520@gmail.com>
2024-01-19 16:17:27 +0900

    [modify]HTTP　HEADのコードを追加

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -16,8 +16,11 @@ public class HttpClientManager
     {
         await GetAsync();
         //await PostAsync();
-        await PostAsJsonAsync();
-        await PutAsync();
+        //await PostAsJsonAsync();
+        //await PutAsync();
+        //await PatchAsync();
+        //await DeleteAsync();
+        await HeadAsync();
     }
 
     private static async Task GetAsync()
@@ -51,41 +54,94 @@ public class HttpClientManager
     //    Console.WriteLine($"{jsonResponse}\n");
     //}
 
-    static async Task PostAsJsonAsync()
+    //static async Task PostAsJsonAsync()
+    //{
+    //    using HttpResponseMessage response = await SharedClient.PostAsJsonAsync(
+    //        "todos",
+    //        new Todo(UserId: 9, Id: 99, Title: "Show extensions", Completed: false));
+
+    //    response.EnsureSuccessStatusCode();
+
+    //    var todo = await response.Content.ReadFromJsonAsync<Todo>();
+    //    Console.WriteLine($"{todo}\n");
+    //}
+
+    //static async Task PutAsync()
+    //{
+    //    using StringContent jsonContent = new(
+    //        JsonSerializer.Serialize(new
+    //        {
+    //            userId = 1,
+    //            id = 1,
+    //            title = "foo bar",
+    //            completed = false
+    //        }),
+    //        Encoding.UTF8,
+    //        "application/json");
+
+    //    using HttpResponseMessage response = await SharedClient.PutAsync(
+    //        "todos/1",
+    //        jsonContent);
+
+    //    response.EnsureSuccessStatusCode();
+
+    //    var jsonResponse = await response.Content.ReadAsStringAsync();
+    //    Console.WriteLine($"{jsonResponse}\n");
+    //}
+
+    //static async Task PatchAsync()
+    //{
+    //    using StringContent jsonContent = new(
+    //        JsonSerializer.Serialize(new { completed = true }),
+    //        Encoding.UTF8,
+    //        "application/json");
+
+    //    var request = new HttpRequestMessage(HttpMethod.Patch, "todos/1") { Content = jsonContent };
+
+    //    using HttpResponseMessage response = await SharedClient.SendAsync(request);
+
+    //    response.EnsureSuccessStatusCode();
+
+    //    var jsonResponse = await response.Content.ReadAsStringAsync();
+    //    Console.WriteLine($"{jsonResponse}\n");
+    //}
+
+    //static async Task DeleteAsync()
+    //{
+    //    using HttpResponseMessage response = await SharedClient.DeleteAsync("todos/1");
+
+    //    response.EnsureSuccessStatusCode();
+
+    //    var jsonResponse = await response.Content.ReadAsStringAsync();
+    //    Console.WriteLine($"{jsonResponse}\n");
+    //}
+
+    static async Task HeadAsync()
     {
-        using HttpResponseMessage response = await SharedClient.PostAsJsonAsync(
-            "todos",
-            new Todo(UserId: 9, Id: 99, Title: "Show extensions", Completed: false));
+         var request = new HttpRequestMessage(HttpMethod.Head, "https://www.example.com");
+
+
+        using HttpResponseMessage response = await SharedClient.SendAsync(request);
 
         response.EnsureSuccessStatusCode();
 
-        var todo = await response.Content.ReadFromJsonAsync<Todo>();
-        Console.WriteLine($"{todo}\n");
+
+        foreach (var header in response.Headers)
+        {
+            Console.WriteLine($"{header.Key}: {string.Join(", ", header.Value)}");
+        }
+        Console.WriteLine();
+
+        // Expected output:
+        //   HEAD https://www.example.com/ HTTP/1.1
+        //   Accept-Ranges: bytes
+        //   Age: 550374
+        //   Cache-Control: max-age=604800
+        //   Date: Wed, 10 Aug 2022 17:24:55 GMT
+        //   ETag: "3147526947"
+        //   Server: ECS, (cha / 80E2)
+        //   X-Cache: HIT
     }
-
-    static async Task PutAsync()
-    {
-        using StringContent jsonContent = new(
-            JsonSerializer.Serialize(new
-            {
-                userId = 1,
-                id = 1,
-                title = "foo bar",
-                completed = false
-            }),
-            Encoding.UTF8,
-            "application/json");
-
-        using HttpResponseMessage response = await SharedClient.PutAsync(
-            "todos/1",
-            jsonContent);
-
-        response.EnsureSuccessStatusCode();
-
-        var jsonResponse = await response.Content.ReadAsStringAsync();
-        Console.WriteLine($"{jsonResponse}\n");
-    }
-
 
 
 }
